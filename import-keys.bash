684f37914e09d19dbd3070e2992edc5ca2cf254d
Dan <devdtc@gmail.com>
2023-12-29 17:06:55 -0800

    Add import message to avoid confusion

--- import-keys.bash
+++ import-keys.bash
@@ -4,6 +4,10 @@ rm -rf ./gpg-keys
 
 gpg --decrypt ./gpg-keys.tar.gz.gpg | tar -xvz ./gpg-keys
 
+echo
+echo "done decrypting, attempting import"
+echo
+
 gpg --import ./gpg-keys/public
 gpg --import --allow-secret-key-import ./gpg-keys/secret
 gpg --import-ownertrust ./gpg-keys/ownertrust
